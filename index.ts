582351ab17bd3e61fbe8206c26b6ccd261afff51
HSM009 <hoseinsirat@gmail.com>
2023-03-10 15:56:10 +0500

    commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+
+console.clear();
+
+import { match } from "assert";
+import chalkAnimation from "chalk-animation";
+import { stat } from "fs";
+import inquirer from "inquirer";
+import Choice from "inquirer/lib/objects/choice.js";
+import chalk from "chalk";
+
+//------------------------------------------------------------------------------
+//FUNCTIONS HERE
+//------------------------------------------------------------------------------
+
+const stopTime = ()=>{
+    return new Promise((res:any)=>{
+        setTimeout(res,3500);
+    })
+}
+
+
+async function welcome() {
+    let rainbowTitle = chalkAnimation.neon("Welcome To ATM Banking!\n\nCoded By Hosein Sirat Mohammad\n");
+    await stopTime();
+    rainbowTitle.stop();
+}
+
+
+//------------------------------------------------------------------------------
+//MAIN
+//------------------------------------------------------------------------------
+
+await welcome();
+
