a45d2ca7f4f3ae5e6f8778c29f612a0f791e865d
HSM009 <hoseinsirat@gmail.com>
2023-03-14 16:32:52 +0500

    All currncy data fixed Second_Branch

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ import chalk from "chalk";
 //------------------------------------------------------------------------------
 //CONTRUCTORS/INTERFACES/CLASSES HERE
 //-----------------------------------------------------------------------,,-------
+let localVariable;
 const currecnyValueToPkr = [
-    { name: 'Pakistani_Rupees', value: '1' },
-    { name: 'US_Dollar', value: '283.50' },
-    { name: 'UK_Pound', value: '336.6' },
-    { name: 'UAE_Dirham', value: '77.00' },
-    { name: 'Saudi_Riyal', value: '75.00' },
-    { name: 'Japenese_Yen', value: '2.06' },
-    { name: 'Euro', value: '299.50' },
-    { name: 'China_Yuan', value: '39.54' },
-    { name: 'Canadian_Dollar', value: '202' },
-    { name: 'Austrailian_Dollar', value: '184.5' }
+    { name: 'Pakistani_Rupee', value: '1', ISO: 'PKR' },
+    { name: 'US_Dollar', value: '283.50', ISO: 'USD' },
+    { name: 'UK_Pound', value: '336.6', ISO: 'GBP' },
+    { name: 'UAE_Dirham', value: '77.00', ISO: 'AED' },
+    { name: 'Saudi_Riyal', value: '75.00', ISO: 'SAR' },
+    { name: 'Japenese_Yen', value: '2.06', ISO: 'JPY' },
+    { name: 'Euro', value: '299.50', ISO: 'EUR' },
+    { name: 'China_Yuan', value: '39.54', ISO: 'CNY' },
+    { name: 'Canadian_Dollar', value: '202', ISO: 'CAD' },
+    { name: 'Austrailian_Dollar', value: '184.5', ISO: 'AUD' }
 ];
-;
+// interface iCurrency {
+//     name: string;
+//     abbreviation: string;
+//     valueToPKR: number
+// };
 //------------------------------------------------------------------------------
 //FUNCTIONS HERE
 //------------------------------------------------------------------------------
@@ -32,7 +37,20 @@ async function welcome() {
     await stopTime();
     rainbowTitle.stop();
 }
+async function localLocation() {
+    var lLocation = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'localSelected',
+            message: 'Select your local area currency:',
+            choices: currecnyValueToPkr.filter((filArr) => filArr.name = filArr.name.replace('_', ' ')).map((filArr) => filArr.name)
+        }
+    ]);
+    localVariable = currecnyValueToPkr.find((val) => val.name == lLocation.localSelected);
+}
+;
 async function mainMenu() {
+    console.log('You have selected local Area currency ' + chalk.inverse(localVariable.name) + '.\n');
     var seletedOption = await inquirer.prompt([
         {
             type: 'list',
@@ -40,11 +58,11 @@ async function mainMenu() {
             message: 'What would you like to do?',
             choices: [
                 {
-                    name: 'Show all currencies and their value data to 1 PKR.',
+                    name: 'Show all currencies and their currency conversion to ' + localVariable.name + ':',
                     value: 'showAllChoice'
                 },
                 {
-                    name: 'Choose custom currenct converter.',
+                    name: 'Choose custom currency converter:',
                     value: 'customConverterChoice'
                 },
                 {
@@ -55,7 +73,7 @@ async function mainMenu() {
         }
     ]);
     if (seletedOption.menuOption == 'showAllChoice') {
-        await showAllToPkr();
+        await showAll();
         mainMenu();
     }
     else if (seletedOption.menuOption == 'customConverterChoice') {
@@ -67,28 +85,70 @@ async function mainMenu() {
     }
 }
 ;
-async function showAllToPkr() {
-    console.log(chalk.bgBlue('\t\tSr-No') + ' >>>>>>>>> ' + (chalk.bgBlue('Currency Name')) + ' >>>>>>>>> ' + (chalk.bgBlue('Value')));
-    const filerArrCurrency = currecnyValueToPkr.filter((fil) => fil.name != 'Pakistani_Rupees');
-    if (filerArrCurrency.length > 0) {
+async function showAll() {
+    //Convert to local here//
+    //-------------------------------------
+    console.log(chalk.bgBlue('\n\t\tSr-No') + ' >>>>>>>>> ' + (chalk.bgBlue('Currency ISO')) + ' = ' + (chalk.bgBlue('Value')));
+    const filerArrCurrency = currecnyValueToPkr.filter((fil) => fil.name != localVariable.name);
+    if (filerArrCurrency.length > 0 && localVariable.ISO == 'PKR') {
         let i = 1;
-        filerArrCurrency.forEach((value) => {
-            console.log(chalk.blue('\t\t    ' + i) + ' >>>>>>>>> ' + (chalk.blue(value.name.replace('_', ' '))) + ' >>>>>>>>> ' + (chalk.blue(value.value)));
+        filerArrCurrency.forEach((val) => {
+            console.log(chalk.blue('\t\t    ' + i) + ' >>>>>>>>> ' + (chalk.blue('1 ' + val.ISO) + '= ' + (chalk.blue(val.value + ' ' + localVariable.ISO))));
             i++;
         });
         console.log('\n');
     }
     else {
-        console.log(chalk.bgYellowBright('No Currency added.\n'));
+        let i = 1;
+        filerArrCurrency.forEach((val) => {
+            let fX = fExchangeRate(val.name);
+            console.log(chalk.blue('\t\t    ' + i) + ' >>>>>>>>> ' + (chalk.blue('1 ' + val.ISO) + '= ' + (chalk.blue(fX + ' ' + localVariable.ISO))));
+            i++;
+        });
+        console.log('\n');
     }
 }
 ;
 async function customChoose() {
+    var cChose = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'select1',
+            message: 'Select the currency to enter:',
+            choices: currecnyValueToPkr.filter((filArr) => filArr.name = filArr.name.replace('_', ' ')).map((filArr) => filArr.name)
+        }
+    ]);
+    var msg = cChoseFunc(cChose.select1);
+    var cVal = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'num1',
+            message: msg,
+            validate: function (value) {
+                if (Number.isInteger(value)) {
+                    return true;
+                }
+                else {
+                    return "Incorrect Input. Must type number.";
+                }
+            }
+        }
+    ]);
+}
+;
+function cChoseFunc(getCurrency) {
+    return 'Enter the amount of ' + getCurrency + '/s:';
+}
+;
+function fExchangeRate(cName) {
+    let num1 = currecnyValueToPkr.find((val) => val.name == localVariable.name);
+    let num2 = currecnyValueToPkr.find((val) => val.name == cName);
+    return (num2.value / num1.value).toFixed(4).toString();
 }
 ;
 //------------------------------------------------------------------------------
 //MAIN HERE
 //------------------------------------------------------------------------------
 // await welcome();
-const arrCurrncy = [];
+await localLocation();
 await mainMenu();
--- index.ts
+++ index.ts
@@ -8,24 +8,24 @@ import { stat } from "fs";
 import inquirer from "inquirer";
 import Choice from "inquirer/lib/objects/choice.js";
 import chalk from "chalk";
-import { CLIENT_RENEG_LIMIT } from "tls";
 import Choices from "inquirer/lib/objects/choices.js";
 
 //------------------------------------------------------------------------------
 //CONTRUCTORS/INTERFACES/CLASSES HERE
 //-----------------------------------------------------------------------,,-------
+let localVariable:any;
 
 const currecnyValueToPkr  = [
- { name:    'Pakistani_Rupees',     value:      '1'},
- { name:    'US_Dollar',            value:      '283.50'},
- { name:    'UK_Pound',             value:      '336.6'},
- { name:    'UAE_Dirham',           value:      '77.00'},
- { name:    'Saudi_Riyal',          value:      '75.00'},
- { name:    'Japenese_Yen',         value:      '2.06'},
- { name:    'Euro',                 value:      '299.50'},
- { name:    'China_Yuan',           value:      '39.54'},
- { name:    'Canadian_Dollar',      value:      '202'},
- { name:    'Austrailian_Dollar',   value:      '184.5'}
+ { name:    'Pakistani_Rupee',      value:      '1',        ISO:    'PKR'},
+ { name:    'US_Dollar',            value:      '283.50',   ISO:    'USD'},
+ { name:    'UK_Pound',             value:      '336.6',    ISO:    'GBP'},
+ { name:    'UAE_Dirham',           value:      '77.00',    ISO:    'AED'},
+ { name:    'Saudi_Riyal',          value:      '75.00',    ISO:    'SAR'},
+ { name:    'Japenese_Yen',         value:      '2.06',     ISO:    'JPY'},
+ { name:    'Euro',                 value:      '299.50',   ISO:    'EUR'},
+ { name:    'China_Yuan',           value:      '39.54',    ISO:    'CNY'},
+ { name:    'Canadian_Dollar',      value:      '202',      ISO:    'CAD'},
+ { name:    'Austrailian_Dollar',   value:      '184.5',    ISO:    'AUD'}
 ];
 
 // interface iCurrency {
@@ -52,7 +52,21 @@ async function welcome() {
     rainbowTitle.stop();
 }
 
+async function localLocation() {
+    var lLocation = await inquirer.prompt([
+        {
+            type:   'list',
+            name:   'localSelected',
+            message:'Select your local area currency:',
+            choices: currecnyValueToPkr.filter((filArr)=> filArr.name = filArr.name.replace('_',' ')).map((filArr) => filArr.name)
+        }
+    ]);
+    localVariable = currecnyValueToPkr.find((val) => val.name == lLocation.localSelected);
+
+};
+
 async function mainMenu() {
+    console.log('You have selected local Area currency ' + chalk.inverse(localVariable.name) + '.\n');
     var seletedOption: any = await inquirer.prompt([
         {
             type:   'list',
@@ -60,11 +74,11 @@ async function mainMenu() {
             message:'What would you like to do?',
             choices: [
                 {
-                    name:   'Show all currencies and their value data to 1 PKR.',
+                    name:   'Show all currencies and their currency conversion to ' + localVariable.name + ':',
                     value:  'showAllChoice'
                 },
                 {
-                    name:   'Choose custom currency converter.',
+                    name:   'Choose custom currency converter:',
                     value:  'customConverterChoice'
                 },
                 {
@@ -77,7 +91,7 @@ async function mainMenu() {
     
     if (seletedOption.menuOption == 'showAllChoice')
     {
-        await showAllToPkr();
+        await showAll();
         mainMenu();
     }
     else if (seletedOption.menuOption == 'customConverterChoice')
@@ -92,39 +106,83 @@ async function mainMenu() {
 
 };
 
-async function showAllToPkr() {
+async function showAll() {
     
-    console.log( chalk.bgBlue('\t\tSr-No') +' >>>>>>>>> '+ (chalk.bgBlue('Currency Name')) + ' >>>>>>>>> '+ (chalk.bgBlue('Value')));
+    //Convert to local here//
 
-    const filerArrCurrency =  currecnyValueToPkr.filter((fil) => fil.name != 'Pakistani_Rupees' )
-    if (filerArrCurrency.length > 0)
+    //-------------------------------------
+    console.log( chalk.bgBlue('\n\t\tSr-No') +' >>>>>>>>> '+ (chalk.bgBlue('Currency ISO')) + ' = '+ (chalk.bgBlue('Value')));
+
+    const filerArrCurrency =  currecnyValueToPkr.filter((fil) => fil.name != localVariable.name )
+    if (filerArrCurrency.length > 0 && localVariable.ISO == 'PKR')
     {
         let i:number = 1;
-        filerArrCurrency.forEach((value) =>
+        filerArrCurrency.forEach((val) =>
         {
-            console.log( chalk.blue('\t\t    '+i) +' >>>>>>>>> '+ (chalk.blue(value.name.replace('_',' '))) + ' >>>>>>>>> '+ (chalk.blue(value.value)));
+            console.log( chalk.blue('\t\t    '+i) +' >>>>>>>>> '+ (chalk.blue('1 ' + val.ISO) + ' = '+ (chalk.blue(val.value+ ' '  +localVariable.ISO))));
             i++;
         })
         console.log('\n');
     }
     else
     {
-        console.log(chalk.bgYellowBright('No Currency added.\n'));
+        let i:number = 1;
+        filerArrCurrency.forEach((val) =>
+        {
+            let fX:any = fExchangeRate(val.name);
+            console.log( chalk.blue('\t\t    '+i) +' >>>>>>>>> '+ (chalk.blue('1 ' + val.ISO) + ' = '+ (chalk.blue( fX + ' '  +localVariable.ISO))));
+            i++;
+        })
+        console.log('\n');
     }
 };
 
 async function customChoose() {
-
-    
+    var cChose = await inquirer.prompt([
+        {
+            type:       'list',
+            name:       'select1',
+            message:    'Select the currency to enter:',
+            choices:    currecnyValueToPkr.filter((filArr)=> filArr.name = filArr.name.replace('_',' ')).map((filArr) => filArr.name)
+            
+        }
+    ]);
+   var msg = cChoseFunc(cChose.select1);
+    var cVal = await inquirer.prompt([
+        {
+            type:       'number',
+            name:       'num1',
+            message:     msg ,
+            validate : function(value) {
+                if(Number.isInteger(value))
+                {
+                    return true;
+                }
+                else 
+                {
+                    return "Incorrect Input. Must type number.";
+                }                    
+            }
+        }
+    ]);
 };
 
+function cChoseFunc(getCurrency:string){
+     return 'Enter the amount of ' + getCurrency + '/s:';
+} ;
+
+function fExchangeRate(cName:string)
+{   
+    let num1:any = currecnyValueToPkr.find((val) => val.name == localVariable.name );
+    let num2:any = currecnyValueToPkr.find((val) => val.name == cName );
+    
+    return  (num2.value/num1.value).toFixed(4).toString()
+};
 
 //------------------------------------------------------------------------------
 //MAIN HERE
 //------------------------------------------------------------------------------
 
 // await welcome();
-
-// const arrCurrncy: iCurrency[] =[];
-
+await localLocation();
 await mainMenu();
